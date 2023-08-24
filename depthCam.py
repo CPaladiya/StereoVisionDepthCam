60c7b21636f0a6bfa067d02173a3345ce0ab45ad
Chirag Paladiya <chirag.paladiya@hotmail.com>
2023-08-23 19:16:58 -0700

    Tuning window added to the depthcam

--- depthCam.py
+++ depthCam.py
@@ -5,57 +5,93 @@ class DepthCam():
     """Class used to start and tune depth camera
     """
     # slots are used for memory optimization
-    __slot__ = ["FOV", "BaseDist", "widthRes", "heightRes", "RThres", "GThres", "BThres"]
+    __slot__ = ["FOV", "BaseDist", "widthRes", "Hmin", "Smin", "Vmin", "Hmax", "Smax", "Vmax", "namedWindow"]
     
     def __init__(self, FOV:float, BaseDist:float, widthRes:int, heightRes:int) -> None:
-        self.FOV        :float  = FOV           # field of view of camera
-        self.BaseDist   :float  = BaseDist      # distance between two cameras
-        self.widthRes   :int    = widthRes      # width resolution of the camera
-        self.heightRes  :int    = heightRes     # height resolution of the camera
-        self.HThres     :int    = 255          # Hue color value threshold
-        self.SThres     :int    = 255          # Sat color value threshold
-        self.VThres     :int    = 255          # Val color value threshold
+        
+        self.FOV            :float  = FOV           # field of view of camera
+        self.BaseDist       :float  = BaseDist      # distance between two cameras
+        self.widthRes       :int    = widthRes      # width resolution of the camera
+        self.heightRes      :int    = heightRes     # height resolution of the camera
+        self.Hmin           :int    = 0             # Hue-min color value threshold
+        self.Smin           :int    = 0             # Sat-min color value threshold
+        self.Vmin           :int    = 0             # Val-min color value threshold
+        self.Hmax           :int    = 179           # Hue-max color value threshold
+        self.Smax           :int    = 255           # Sat-max color value threshold
+        self.Vmax           :int    = 255           # Val-max color value threshold
+        self.namedWindow    :str    = 'Feed'
     
     
-    def tune(self, cam1:int=0, cam2:int=1, H:bool=False, S:bool=False, V:bool=False) -> None:
-        """Sets value of threshold for red, green and blue value.
-        Function assumes that two cameras are connected and they are not built in pc webcams.
+    def tune(self, cam1:int=0, cam2:int=1) -> None:
+        """Sets value of threshold for Hue, Saturation and Value channels. It requires exactly two cameras.
+        The video feed can be switch left to right by swapping cam1 and cam2 id.
 
         Args:
             cam1 (int, optional): Device id of first camera. Defaults to 0.
             cam2 (int, optional): Device id of second camera. Defaults to 1.
-            H (bool, optional): set `True` to tune tuning of Hue channel. Defaults to False.
-            S (bool, optional): set `True` to tune tuning of Sat channel. Defaults to False.
-            V (bool, optional): set `True` to tune tuning of Val channel. Defaults to False.
         """ 
-            
         # starting a video feed
-        camCaputre1 = self.OpenCameraFeed(cam1)
-        camCaputre2 = self.OpenCameraFeed(cam2)
-        hmin, smin, vmin = 0,0,0
-        hmax, smax, vmax = 179,255,255
-        
-        def on_trackbar(val):
-            print(val)
-            
-        cv2.namedWindow("HSVTrackBars")
-        cv2.createTrackbar("HueMin", "HSVTrackBars", 0, 179, on_trackbar)
-        
+        self.startTrackers()
+        camCaputre1 = self.openCameraFeed(cam1)
+        camCaputre2 = self.openCameraFeed(cam2)
         while True:
-            
             frame1 = self.retriveFrame(camCaputre1)
             frame2 = self.retriveFrame(camCaputre2)
-
-            cv2.imshow(f"camId_{cam1}",frame1)
-            cv2.imshow(f"camId_{cam2}",frame2)
+            # connecting two frames into one
+            frame = np.concatenate([frame1, frame2], axis=1)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            frame = cv2.inRange(frame, (self.Hmin, self.Smin, self.Vmin), (self.Hmax, self.Smax, self.Vmax))
+            # adding text to the image to quit once done tuning
+            frame = cv2.putText(
+                                    img = frame,
+                                    text = "press `q` once done tuning!",
+                                    org = (10, 50),
+                                    fontFace = cv2.FONT_HERSHEY_DUPLEX,
+                                    fontScale = 1.5,
+                                    color = (88, 86, 93),
+                                    thickness = 3)
+            cv2.imshow(self.namedWindow,frame)
+            # quit when `q` is pressed
             if cv2.waitKey(1) == ord('q'):
                 break
         camCaputre1.release()
         camCaputre2.release()
         cv2.destroyAllWindows()
         
+    
+    def startTrackers(self) -> None:
+        """sets trackers on the main camera feed window
+        """
+        # setting up sliders and tuning parameters
+        def on_trackbar_hmin(val):
+            nonlocal self
+            self.Hmin = val
+        def on_trackbar_hmax(val):
+            nonlocal self
+            self.Hmax = val
+        def on_trackbar_smin(val):
+            nonlocal self
+            self.Smin = val
+        def on_trackbar_smax(val):
+            nonlocal self
+            self.Smax = val
+        def on_trackbar_vmin(val):
+            nonlocal self
+            self.Vmin = val
+        def on_trackbar_vmax(val):
+            nonlocal self
+            self.Vmax = val
+        cv2.namedWindow(self.namedWindow)
+        # by default minimum value of slider is always 0
+        cv2.createTrackbar("HueMin", self.namedWindow, 20, 179, on_trackbar_hmin)
+        cv2.createTrackbar("HueMax", self.namedWindow, 150, 179, on_trackbar_hmax)
+        cv2.createTrackbar("SatMin", self.namedWindow, 20, 255, on_trackbar_smin)
+        cv2.createTrackbar("SatMax", self.namedWindow, 150, 255, on_trackbar_smax)
+        cv2.createTrackbar("ValMin", self.namedWindow, 20, 255, on_trackbar_vmin)
+        cv2.createTrackbar("ValMax", self.namedWindow, 150, 255, on_trackbar_vmax)
         
-    def OpenCameraFeed(self,cam:int) -> cv2.VideoCapture:
+    
+    def openCameraFeed(self,cam:int) -> cv2.VideoCapture:
         """Open a camera feed and performs essential checks
 
         Args:
@@ -74,6 +110,7 @@ class DepthCam():
         if not camCaputre.isOpened():
             raise BrokenPipeError(f"camera {cam} feed could not be opened!")
         return camCaputre
+    
     
     def retriveFrame(self, cameraFeed:cv2.VideoCapture) -> np.ndarray:
         """Retrive the frame from the camera feed
